220184a20547a6ede4112fe8ff7ba5d5e0914e39
saltegg <805299748@qq.com>
2021-11-19 12:52:25 +0800

    test

--- src/fzu/com/ArrayTest.java
+++ src/fzu/com/ArrayTest.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+        int[] a = new int[] { 18, 62, 68, 82, 65, 9 };
         String content = Arrays.toString(a);
         System.out.println(content);
+
     }
 }
